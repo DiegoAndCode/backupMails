03026ad0bed42573a4d7048d3192b8d6be2ca26a
DiegoAndCode <diegodeandrade1986@gmail.com>
2024-04-12 09:04:41 -0300

    .

--- src/app.py
+++ src/app.py
@@ -5,6 +5,9 @@ import mail_bkpcompress
 backup = mail_backup.Backup()
 backup.run_backup()
 
+print('*' * 10, 'Backup concluído', '*' * 10)
+print('*' * 10, 'Iniciando compressão dos arquivos', '*' * 10)
+
 # Comprimir arquivos de backup .mbox
 compress = mail_bkpcompress.FileCompression()
 compress.run_compress()
